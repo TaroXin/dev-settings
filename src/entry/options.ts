2a7fccef2fa0fb347ac98eeed47121c8062ab30b
TaroXin <15029352778@163.com>
2023-01-09 17:26:10 +0800

    feat: add options.html

--- src/entry/options.ts
+++ src/entry/options.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from '@/view/options.vue'
+import 'normalize.css'
 
 createApp(App).mount('#app')
--- src/view/options.vue
+++ src/view/options.vue
@@ -1,27 +1,68 @@
-<script lang="ts">
-export default {
-  name: 'OptionsView',
-  data() {
-    return {
-      msg: 'options',
-    }
+<script lang="ts" setup>
+import { computed, reactive, ref } from 'vue'
+import VarList from '@/components/var-list/index.vue'
+import Settings from '@/components/settings/index.vue'
+
+const menuList = ref([
+  {
+    name: '变量列表',
+    component: computed(() => VarList),
   },
-}
+  {
+    name: '设置',
+    component: computed(() => Settings),
+  },
+])
+const currentPage = ref(0)
 </script>
 
 <template>
-  <div class="main_app">
-    <h1>Hello {{ msg }}</h1>
+  <div class="menu">
+    <div class="menu-header">
+      <div
+        v-for="(menu, index) in menuList"
+        :key="index"
+        class="menu-header-item"
+        :class="{ active: index == currentPage }"
+        @click="currentPage = index"
+      >
+        {{ menu.name }}
+      </div>
+    </div>
+    <div class="menu-content">
+      <component
+        :is="menuList[currentPage]?.component"
+      />
+    </div>
   </div>
 </template>
 
 <style lang="less" scoped>
-.main_app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.menu {
+  width: 1024px;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  padding: 20px 0;
+  font-size: 14px;
+  box-sizing: border-box;
+
+  &-header {
+    width: 200px;
+
+    &-item {
+      padding: 15px;
+      cursor: pointer;
+
+      &.active, &:hover {
+        background: rgba(255,255,255,0.6);
+      }
+    }
+  }
+
+  &-content {
+    flex: 1;
+    background: rgba(255,255,255,0.6);
+  }
 }
 </style>
